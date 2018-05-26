6621ac3c55c6ac4a3186a28b992542f80de864df
shishir99111 <shishir99111@gmail.com>
2018-05-26 11:16:09 +0530

    socket initialization

--- config/socket.io.js
+++ config/socket.io.js
@@ -1,9 +1,25 @@
 let io = null;
 
-module.exports = (server) => {
+function creatServer() {
   if (io != null) {
     return io;
   }
   io = require('socket.io')(server); // eslint-disable-line
   return io;
+}
+
+function closeServer() {
+  if (io != null) {
+    return io;
+  }
+  io = require('socket.io')(server); // eslint-disable-line
+  return io;
+}
+
+module.exports = (server) => {
+  this.server = server;
+  return {
+    creatServer,
+    closeServer,
+  };
 };
